89ee58fdba155c9240d2277904bf914e4b0b74a2
akurafamily <akurafamily@gmail.com>
2025-05-05 10:01:17 +0530

    Add blackboxai-1746243407399/astrobalendar/apps/frontend/src/App.tsx 1746419477159

---
+++ blackboxai-1746243407399/astrobalendar/apps/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+// TODO: Root app shell with Router setup
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      {/* TODO: Add Router and main layout */}
+      <h1>AstroBalendar Frontend</h1>
+    </div>
+  );
+}
+
+export default App;
